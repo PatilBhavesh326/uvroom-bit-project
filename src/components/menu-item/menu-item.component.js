106aceffaae7ac2a54ef425b9fb90211116291be
PatilBhavesh326 <bhaveshpatil326@gmail.com>
2021-07-11 06:28:47 -0400

    Initial implmentation of Idea

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl}) => {
+    return (
+        <div className='menu-item'>
+
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            />  
+            <div className='title'>
+                <h1>{title.toUpperCase()}</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default MenuItem;
---
+++ src/pages/shop-page/shop-page.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './shop-page.styles.scss'
+const ShopPage = () =>{
+    return(
+        <div className='shoppage'>
+            <p>This is Shoppage</p>
+        </div>
+    )
+}
+
+export default ShopPage;
